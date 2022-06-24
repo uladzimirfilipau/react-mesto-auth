8936f2dcf0eda941a2c4a9e6daf1ffc7e1d92afa
Vova <uladzimirfilipau@gmail.com>
2022-06-24 10:29:52 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   //</React.StrictMode>
