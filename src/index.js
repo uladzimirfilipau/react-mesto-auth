d6708b87b6f882e881b78e7d989fe39ed77952e5
Vova <uladzimirfilipau@gmail.com>
2022-06-24 10:02:26 +0300

    feat: BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 import './index.css';
@@ -8,8 +8,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/uladzimirfilipau.github.io/mesto-react-auth">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   //</React.StrictMode>
 );
