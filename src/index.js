5d2a0f542e79f238e3ba33d61b48bb385dade1eb
Vova <uladzimirfilipau@gmail.com>
2022-06-24 11:44:01 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={'/mesto-react-auth'}>
       <App />
     </BrowserRouter>
   //</React.StrictMode>
