c493868a78336f06e119367bea92673a7862f217
Vova <uladzimirfilipau@gmail.com>
2022-06-24 10:16:47 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <BrowserRouter basename="/uladzimirfilipau.github.io/mesto-react-auth">
+    <BrowserRouter basename={window.location.pathname || ''}>
       <App />
     </BrowserRouter>
   //</React.StrictMode>
