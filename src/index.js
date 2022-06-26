339c8c22de69d92e194b85d12d07013283ee56e1
Vova <uladzimirfilipau@gmail.com>
2022-06-26 09:09:39 +0300

    update Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- // <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+  // <React.StrictMode>
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </HashRouter>
   //</React.StrictMode>
 );
