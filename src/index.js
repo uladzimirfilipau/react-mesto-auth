2de13c45d913bbc99f9f3d6a1c2cd61dfcccd60f
Vova <uladzimirfilipau@gmail.com>
2022-06-26 11:41:24 +0300

    update Router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <App />
   </HashRouter>
   //</React.StrictMode>
