5602ecd047f735973b4309e6bbd0447445e74010
Vova <uladzimirfilipau@gmail.com>
2022-06-30 15:27:23 +0300

    update react-router

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
